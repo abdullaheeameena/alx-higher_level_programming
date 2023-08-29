27bb799895524eb44de09226cf8b97ce45bc5aeb
amina <abdullaheeameena@gmail.com>
2023-08-29 02:59:14 -0700

    we

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"Define class Square."
+
+
+class Square:
+
+    "Represent what a square is."
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"Defines what is  a class Square."
+
+
+class Square:
+
+    "Represent the definition of a class square."
+    def __init__(self, size):
+        "Initialize a new Square.
+        Args:
+            size(int): The size of the new square.
+        "
+        self.__size = size
